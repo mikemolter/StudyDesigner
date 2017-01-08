bc3a4d1030ff6917e5882630e7532202b62e4360
Mike Molter <mikemolter@Mikes-MacBook-Pro.local>
2017-01-08 14:31:39 -0500

    Add Django project

---
+++ SDDjangoProject/CTVag/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url('DBCodelists',views.dbcodelists),
+    url('Child',views.ChildrenCL),
+    url('Back2DBCodeList',views.Back2DBCodeList)
+]
